0e3129b649c478d576be76d0933502f831126c11
BugHunter-X <askzenai@gmail.com>
2025-01-22 23:12:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo `Camera` component with custom styling.  The preview image might not render correctly, appearing stretched or distorted, especially on devices with different aspect ratios. This is often due to conflicting styles or incorrect sizing of the Camera component's container.
